c78e84ed878b9a555dfb04dadbd50fd2896b8b31
Björn Tropf <btropf@asym.local>
2016-04-15 19:35:43 +0200

    Build a testcase for development

--- src/Jlc.hs
+++ src/Jlc.hs
@@ -3,17 +3,34 @@ module Main where
 import System.IO ( stdin, hGetContents, hPutStrLn, stderr )
 import System.Environment ( getArgs, getProgName )
 import System.Exit ( exitFailure, exitSuccess )
+import Control.Monad (when)
 
 import Javalette.Lex
 import Javalette.Par
 import Javalette.Skel
 import Javalette.Print
 import Javalette.Abs
-
+import Javalette.ErrM
 import Frontend.TypeCheck
+
+type Verbosity = Int
+type ParseFun a = [Token] -> Err a
+
+putStrV :: Verbosity -> String -> IO ()
+putStrV v s = when (v > 1) $ putStrLn s
+
+parseFile :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
+parseFile v p s = let ts = myLexer s in case p ts of
+             Bad s    -> do putStrLn "\nParse failed...\n"
+                            exitFailure
+             Ok  tree -> do putStrLn "\nParse successful!\n"
+                            putStrV v $ show tree
+                            putStrLn ""
+                            exitSuccess
 
 main :: IO ()
 main = do
+  getContents >>= parseFile 2 pProgram
   typeCheck
   hPutStrLn stderr "ERROR"
   exitFailure
