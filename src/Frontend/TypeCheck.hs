06bebab9cc4f288804ca5906361c545fc7ca8a17
Björn Tropf <bjoerntropf@gmail.com>
2016-04-16 12:55:00 +0200

    Partly implement a few more functions.

--- src/Frontend/TypeCheck.hs
+++ src/Frontend/TypeCheck.hs
@@ -27,30 +27,44 @@ remBlock (s, c)  = Ok (s, tail c)
 lookupVar :: Env -> Ident -> Maybe Type
 lookupVar (_, []) _ = Nothing
 lookupVar (_, c)  i = lookupV c where
-  lookupV (t:r) = case Map.lookup i t of
-    Nothing -> lookupV r
-    Just y  -> return y
+    lookupV (t:r) = case Map.lookup i t of
+        Nothing -> lookupV r
+        Just y  -> return y
 
 lookupFun :: Env -> Ident -> Maybe ([Type],Type)
 lookupFun (s, _) i = Map.lookup i s
 
 extendVar :: Env -> Ident -> Type -> Err Env
 extendVar (s, t:r) i y = case Map.lookup i t of
-  Nothing -> Ok (s, Map.insert i y t:r)
-  Just _  -> Bad "The variable is already defined."
+    Nothing -> Ok (s, Map.insert i y t:r)
+    Just _  -> Bad ("The variable is already defined." ++ show i)
 
 extendFun :: Env -> Ident -> ([Type],Type) -> Err Env
 extendFun (s, c) i y = case Map.lookup i s of
-  Nothing -> Ok (Map.insert i y s, c)
-  Just _  -> Bad "The function is already defined."
+    Nothing -> Ok (Map.insert i y s, c)
+    Just _  -> Bad ("The function is already defined." ++ show i)
 
--- infer :: Env -> Expr -> Type
+inferBin :: [Type] -> Env -> Expr -> Expr -> Err Type
+inferBin t e x1 x2 = do
+    y <- inferExpr e x1
+    if y `elem` t
+        then checkExpr e x2 y
+        else Bad ("Wrong type of expression: " ++ show y)
 
--- check :: Env -> Expr -> Type -> Err Bool?
+inferExpr :: Env -> Expr -> Err Type
+inferExpr e x = case x of
+    ELitInt i  -> return Int
+    ELitDoub d -> return Doub
+    ELitTrue   -> return Bool
+    ELitFalse  -> return Bool
+    EAdd x1 addop x2 -> inferBin [Int, Doub] e x1 x2
+
+checkExpr :: Env -> Expr -> Type -> Err Type
+checkExpr e x y = Ok Bool
+
+-- check :: Env -> Expr  -> Err Type?
 
 -- check :: Env -> [Stmt] -> Err Bool?
-
--- check :: Env -> Def -> Err Bool?
 
 -- check :: Program -> Err Bool?
 
