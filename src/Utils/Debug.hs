ffda84493e3bfcf8db6ca1f4531e4fbca7f2ef53
Mazdak Farrokhzad <twingoow@gmail.com>
2016-04-16 21:48:26 +0200

    added Utils.Debug

---
+++ src/Utils/Debug.hs
@@ -0,0 +1,24 @@
+{- Javalette Compiler
+   Copyright, 2016, Björn Tropf, Mazdak Farrokhzad
+ -}
+
+{-|
+Module      : Debug
+Description : Debugging of stuff.
+Copyright   : (c) Björn Tropf, 2016
+                  Mazdak Farrokhzad, 2016
+Stability   : unsafe
+Portability : ALL
+
+Debugging of stuff.
+-}
+module Debug where
+
+import System.IO.Unsafe
+
+-- | 'debug': this function is morally dubious to use,
+-- it lacks referential transparency and might destroy your computer,
+-- set your house on fire, create black holes,
+-- without it saying so in the type signature.
+debug :: Show a => a -> a
+debug x = unsafePerformIO $ print x >> return x
