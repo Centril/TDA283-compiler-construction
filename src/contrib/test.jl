c2ed3e9eae89a09a3186c227af23f1dac6304f53
Björn Tropf <bjoerntropf@gmail.com>
2016-04-17 18:56:01 +0200

    contrib/test.jl: Add additional example for returncheck

--- src/contrib/test.jl
+++ src/contrib/test.jl
@@ -1,3 +1,20 @@
-int f(int x, int x) {
-   return x;
+int main() {
+    return 1;
 }
+
+/*
+    boolean b1 = true;
+    boolean b2 = false;
+    boolean b3 = 1 > 1;
+    boolean b4 = 1.5 == 1.5;
+    boolean b5 = a < b;
+    boolean b6 = true && true;
+    boolean b7 = true || false;
+    boolean b8 = !true;
+    boolean b9 = -1 < 5;
+    boolean b10 = 5 * 5 == 25;
+    boolean b11 = 5 + 1 < 7;
+    boolean b12 = 5 % 5 == 0;
+    boolean b13 = 15 / 5 == 3;
+    boolean b14 = 15 / 0 == 3;
+*/
