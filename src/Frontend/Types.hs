6ed6b8ab006de8442fd2f368cd26f58563a0c262
Mazdak Farrokhzad <twingoow@gmail.com>
2016-04-19 20:36:14 +0200

    Frontend.Types: added bunch of environment operations + cabal: added safe library

--- src/Frontend/Types.hs
+++ src/Frontend/Types.hs
@@ -12,49 +12,127 @@ Portability : ALL
 
 Types for Frontend of Javalette compiler.
 -}
-{-# LANGUAGE GeneralizedNewtypeDeriving,FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Frontend.Types (
     -- * Types
     Env, FnSig, FnSigId, Sig, Err, Log,
 
-    TCEnv, Eval, EvalResult,
-    ErrMsg, InfoLog, LogItem,
+    TCEnv (..), Eval, EvalResult,
+    ErrMsg, InfoLog, LogItem(..),
     Context, Contexts,
-    FunSig, FnId, FnSigMap,
+    FunSig(..), FnId(..), FnSigMap,
 
     -- * Operations
-    runEval, warn, warn', info, info',
+    runEval, warn, warn', info, info', err,
+    initialEnv, pushBlock, popBlock,
+    lookupVar', lookupFun', extendVar', extendFun',
+    functions, contexts,
 ) where
 
-import Data.Map
+import Safe
 
+import Data.Maybe
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import Control.Monad()
+import Control.Applicative()
 import Control.Monad.Identity
 import Control.Monad.State
 import Control.Monad.Except
 import Control.Monad.Writer
 
+import Control.Lens hiding (Context, contexts)
+
+import Utils.Foldable
+import Utils.Monad
+
 import Javalette.Abs
 
 --------------------------------------------------------------------------------
--- OLD: @TODO REMOVE
+-- Scopes / Contexts:
 --------------------------------------------------------------------------------
-type Err =     Either ErrMsg
-type Log =     String
-type FnSig =   ([Type], Type)
-type FnSigId = (Ident, FnSig)
-type Sig =     Map Ident FnSig
-type Env =     (Sig, [Context])
+
+-- | 'Context': A context for a scope, map from variables -> types.
+type Context = Map Ident Type
+
+-- | 'Contexts': List of 'Context'
+type Contexts = [Context]
 
 --------------------------------------------------------------------------------
--- Computational & Operating Environment:
+-- Function Signatures:
+--------------------------------------------------------------------------------
+
+-- | 'FunSig': Signature of a function,
+-- argument list (types) followed by return type.
+data FunSig = FunSig { targs :: [Type], tret :: Type }
+
+-- | 'FnId': Signature of a function ('FunSig') + 'Ident'.
+data FnId = FunId { fident :: Ident, fsig :: FunSig }
+
+-- | 'FnSigMap': Map of function identifiers -> signatures.
+type FnSigMap = Map Ident FunSig
+
+--------------------------------------------------------------------------------
+-- Operating Environment:
 --------------------------------------------------------------------------------
 
 -- | 'TCEnv': The operating environment of an 'Eval' computation.
 data TCEnv = TCEnv {
-    functions :: FnSigMap, -- ^ Map of ident -> function signatures.
-    contexts  :: Contexts  -- ^ Stack of contexts.
+    _functions :: FnSigMap, -- ^ Map of ident -> function signatures.
+    _contexts  :: Contexts  -- ^ Stack of contexts.
 }
+makeLenses ''TCEnv
+
+-- | 'initialEnv': The initial empty environment.
+initialEnv :: TCEnv
+initialEnv = TCEnv Map.empty [Map.empty]
+
+-- | 'pushBlock': pushes a fresh and empty block to the 'Context' stack.
+pushBlock :: Eval ()
+pushBlock = contexts %= (Map.empty:)
+
+-- | 'popBlock': pops the top block from the 'Context' stack.
+popBlock :: Eval ()
+popBlock = contexts %= (fromMaybe [] . tailMay)
+
+-- | 'lookupVar': If var exists in any scope in the 'Contexts', the 'Type' of
+-- the identifier is 'return':ed, otherwise onErr is given the (var = 'Ident').
+lookupVar' :: (Ident -> Eval Type) -> Ident -> Eval Type
+lookupVar' onErr var = uses contexts (_lookupVar var) >>= maybeErr (onErr var)
+
+_lookupVar :: Ident -> Contexts -> Maybe Type
+_lookupVar = mfind . Map.lookup
+
+-- | 'lookupFun': If function with given identifier exists, the 'FunSig' of it
+-- is 'return':ed, otherwise, onErr is given the (var = 'Ident').
+lookupFun' :: (Ident -> Eval FunSig) -> Ident -> Eval FunSig
+lookupFun' onErr var = uses functions (Map.lookup var) >>= maybeErr (onErr var)
+
+-- | 'extendVar': Extends the current scope with the given variable with ident
+-- as 'Ident' and typ as 'Type'. If variable exists, onError is used.
+extendVar' :: (Ident -> Eval ()) -> Ident -> Type -> Eval ()
+extendVar' onErr ident typ = do
+    (c : ctxs) <- use contexts
+    maybe (contexts .= Map.insert ident typ c:ctxs)
+          (const $ onErr ident)
+          (Map.lookup ident c)
+
+-- | 'extendVar': Extends the accumulated function signatures with the given
+-- signature as 'FnId'. If function exists, onError is used.
+extendFun' :: (Ident -> Eval ()) -> FnId -> Eval ()
+extendFun' onErr (FunId ident sig) = do
+    funs <- use functions
+    maybe (functions .= Map.insert ident sig funs)
+          (const $ onErr ident)
+          (Map.lookup ident funs)
+
+--------------------------------------------------------------------------------
+-- Computations in compiler:
+--------------------------------------------------------------------------------
 
 -- | 'Eval': A computation given typechecker state environment 'TCEnv',
 -- potential fail-fast errors of type 'ErrMsg' and accumulated 'InfoLog's.
@@ -98,6 +176,9 @@ type InfoLog = [LogItem]
 -- | 'LogItem': Type of a recoverable error.
 data LogItem = Warn { _warn :: String } | Info { _info :: String }
 
+err :: String -> Eval a
+err = throwError
+
 _log :: MonadWriter [t] m => t -> m ()
 _log w = tell [w]
 
@@ -110,25 +191,11 @@ warn' = warn . unwords
 info' = info . unwords
 
 --------------------------------------------------------------------------------
--- Scopes / Contexts:
+-- OLD: @TODO REMOVE
 --------------------------------------------------------------------------------
-
--- | 'Context': A context for a scope, map from variables -> types.
-type Context = Map Ident Type
-
--- | 'Contexts': List of 'Context'
-type Contexts = [Context]
-
---------------------------------------------------------------------------------
--- Function Signatures:
---------------------------------------------------------------------------------
-
--- | 'FunSig': Signature of a function,
--- argument list (types) followed by return type.
-data FunSig = FunSig { targs :: [Type], tret :: Type }
-
--- | 'FnId': Signature of a function ('FunSig') + 'Ident'.
-data FnId = FnId { fident :: Ident, fsig :: FnSig }
-
--- | 'FnSigMap': Map of function identifiers -> signatures.
-type FnSigMap = Map Ident FnSig
+type Err =     Either ErrMsg
+type Log =     String
+type FnSig =   ([Type], Type)
+type FnSigId = (Ident, FnSig)
+type Sig =     Map Ident FnSig
+type Env =     (Sig, [Context])
