e95e1b91592f7a7b2c49bca57f0f2e627844e8ee
Mazdak Farrokhzad <twingoow@gmail.com>
2016-04-20 17:53:30 +0200

    Jlc: using Frontend v2 logic completely now

--- src/Jlc.hs
+++ src/Jlc.hs
@@ -12,40 +12,45 @@ import Frontend.Typecheck2
 import Utils.Terminal
 import Utils.Debug
 
-output :: Show a => a -> IO ()
-output = putStrLn . prettify . show
+main :: IO ()
+main = getArgs >>= handleArgs >>= compileUnit
 
 handleArgs :: [String] -> IO String
 handleArgs [] = getContents
 handleArgs (f:_) = readFile f
 
-parserPhase :: String -> IO ()
-parserPhase code = case parseProgram code of
-    Left err  -> do
-        errLn "SYNTAX ERROR"
-        putStrLn err
-        exitFailure
-    Right tree -> do
-        putStrLn ""
-        output tree
-        typeCheckPhase tree
-
-typeCheck' :: Program -> EvalResult Program
-typeCheck' prog = runEval (typeCheck prog) initialEnv
-
-typeCheckPhase :: Program -> IO ()
-typeCheckPhase prog = case result of
-    Left err -> do
-        errLn "TYPE ERROR"
-        putStrLn err
+compileUnit :: String -> IO ()
+compileUnit code =
+    case  result of
+    Left  except  -> do
+        let phase = errPhase except
+        putStrLn $ unwords ["ERROR!", "[" ++ show phase ++ "]", errMsg except]
         output logs
+        errLn $ phaseErr phase
         exitFailure
-    Right (ast, env) -> do
-        output logs
-        output ast
-        output env
+    Right (val, env) -> do
+        putStrLn "COMPILATION SUCCESS!"
+        putStrLn "Accumulated logs:"        >> output logs
+        putStrLn "Final computed value:"    >> output val
+        putStrLn "Final environment value:" >> output env
         errLn "OK"
-    where (result, logs) = typeCheck' prog
+    where (result, logs) = runCompileComp code
 
-main :: IO ()
-main = getArgs >>= handleArgs >>= parserPhase
+output :: Show a => a -> IO ()
+output = putStrLn . prettify . show
+
+phaseErr :: Phase -> String
+phaseErr Parser        = "SYNTAX ERROR"
+phaseErr TypeChecker   = "TYPE ERROR"
+phaseErr ReturnChecker = "TYPE ERROR"
+
+runCompileComp :: String -> EvalResult Program
+runCompileComp code = runEval (compileComp code) initialEnv
+
+compileComp :: String -> Eval Program
+compileComp code = do
+    ast1 <- parseProgram code
+    infoln Parser      ["AST after parse:",      show ast1]
+    ast2 <- typeCheck ast1
+    infoln TypeChecker ["AST after type check:", show ast2]
+    return ast2
