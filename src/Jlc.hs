d74093313287719563399133dbfc32b280373f6e
Mazdak Farrokhzad <twingoow@gmail.com>
2016-04-20 15:17:54 +0200

    Jlc: semi cleanup

--- src/Jlc.hs
+++ src/Jlc.hs
@@ -12,36 +12,40 @@ import Frontend.Typecheck2
 import Utils.Terminal
 import Utils.Debug
 
+output :: Show a => a -> IO ()
+output = putStrLn . prettify . show
+
 handleArgs :: [String] -> IO String
 handleArgs [] = getContents
 handleArgs (f:_) = readFile f
 
 parserPhase :: String -> IO ()
-parserPhase s = case parseProgram s of
+parserPhase code = case parseProgram code of
     Left err  -> do
         errLn "SYNTAX ERROR"
         putStrLn err
         exitFailure
     Right tree -> do
         putStrLn ""
-        putStrLn $ prettify $ show tree
+        output tree
         typeCheckPhase tree
 
-tc prog = runEval (typeCheck prog) initialEnv
+typeCheck' :: Program -> EvalResult Program
+typeCheck' prog = runEval (typeCheck prog) initialEnv
 
 typeCheckPhase :: Program -> IO ()
-typeCheckPhase p = case result of
+typeCheckPhase prog = case result of
     Left err -> do
         errLn "TYPE ERROR"
         putStrLn err
-        putStrLn $ prettify $ show logs
+        output logs
         exitFailure
     Right (ast, env) -> do
-        putStrLn $ prettify $ show logs
-        putStrLn $ prettify $ show ast
-        putStrLn $ prettify $ show env
+        output logs
+        output ast
+        output env
         errLn "OK"
-    where (result, logs) = tc p
+    where (result, logs) = typeCheck' prog
 
 main :: IO ()
 main = getArgs >>= handleArgs >>= parserPhase
