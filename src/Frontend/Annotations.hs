32ef61b65780c78c130f37dc6840203bcbf0d330
Mazdak Farrokhzad <twingoow@gmail.com>
2016-04-22 02:39:20 +0200

    Frontend.Annotations: documentation, small fix, lenses, prisms

--- src/Frontend/Annotations.hs
+++ src/Frontend/Annotations.hs
@@ -39,7 +39,9 @@ module Frontend.Annotations (
 
     -- * Operations
     toWillExecute, showKind, emptyAnot, applyEA,
-    _LBool, _LInt, _LDouble, _LString
+    _LBool, _LInt, _LDouble, _LString,
+    litBool, litDouble, litInt, litStr,
+    anotCExprLit, anotKind, anotType, anotWillExec
 ) where
 
 import Control.Lens hiding (Context, contexts)
@@ -60,6 +62,7 @@ data Kind = KConcrete | KConstraint |
             KArrow { kaFrom :: Kind, kaTo :: Kind }
     deriving (Eq, Show, Read, Ord)
 
+-- | 'showKind': pretty prints the 'Kind' using haskell convention.
 showKind :: Kind -> String
 showKind KConcrete    = "*"
 showKind KConstraint  = "Constraint"
@@ -72,14 +75,21 @@ showKind (KArrow f t) = unwords [showf, "->", show t]
 -- Literal & WillExecute:
 --------------------------------------------------------------------------------
 
-data Literal = LBool Bool | LInt Integer | LDouble Double | LString String
+-- | 'Literal': Annotation for the literal that constant expressions result in.
+data Literal = LBool   { _litBool   :: Bool    }    |
+               LInt    { _litInt    :: Integer } |
+               LDouble { _litDouble :: Double }  |
+               LString { _litStr    :: String }
     deriving (Eq, Show, Read, Ord)
 
+makeLenses ''Literal
 makePrisms ''Literal
 
+-- | 'WillExecute': Annotation for when statements will or will not be executed.
 data WillExecute = Always | Never | Unknown
     deriving (Eq, Show, Read, Ord, Enum)
 
+-- | 'toWillExecute': Convert from 'Maybe' 'Bool' to 'WillExecute'.
 toWillExecute :: Maybe Bool -> WillExecute
 toWillExecute (Just True)  = Always
 toWillExecute (Just False) = Never
@@ -89,19 +99,26 @@ toWillExecute Nothing      = Unknown
 -- Annotations:
 --------------------------------------------------------------------------------
 
-data ASTAnot = AType     { _anotType     :: Type ASTAnot } |
-               AWillExec { _anotWillExec :: WillExecute  } |
-               ACExprLit { _anotCExprLit :: Literal      } |
-               AKind     { _anotKind     :: Kind         }
+-- | 'ASTAnot': The annotations allowed in a Javalette AST.
+data ASTAnot = AType     { _anotType     :: Type [ASTAnot] } |
+               AWillExec { _anotWillExec :: WillExecute    } |
+               ACExprLit { _anotCExprLit :: Literal        } |
+               AKind     { _anotKind     :: Kind           }
     deriving (Eq, Show, Read)
+
+makePrisms ''ASTAnot
+makeLenses ''ASTAnot
 
 -- | 'ASTAnot': Annotations added to a 'Program' AST which can be safely
 -- 'void':ed away.
 type ASTAnots = [ASTAnot]
 
+-- | 'emptyAnot': no annotations, for nodes where annotations have no meaning.
 emptyAnot :: ASTAnots
 emptyAnot = []
 
+-- | 'applyEA': given a node constructor that accepts an 'ASTAnots',
+-- partially applies the empty annotation to it.
 applyEA :: [ASTAnots -> a] -> [a]
 applyEA = fmap ($ emptyAnot)
 
@@ -109,14 +126,35 @@ applyEA = fmap ($ emptyAnot)
 -- AST Annotations, Aliases:
 --------------------------------------------------------------------------------
 
+-- | 'ProgramA': 'Program' annotated with 'ASTAnots'.
 type ProgramA = Program ASTAnots
+
+-- | 'TopDefA': 'TopDef' annotated with 'ASTAnots'.
 type TopDefA  = TopDef  ASTAnots
+
+-- | 'ArgA': 'Arg' annotated with 'ASTAnots'.
 type ArgA     = Arg     ASTAnots
+
+-- | 'BlockA': 'Block' annotated with 'ASTAnots'.
 type BlockA   = Block   ASTAnots
+
+-- | 'StmtA': 'Stmt' annotated with 'ASTAnots'.
 type StmtA    = Stmt    ASTAnots
+
+-- | 'ItemA': 'Item' annotated with 'ASTAnots'.
 type ItemA    = Item    ASTAnots
+
+-- | 'TypeA': 'Type' annotated with 'ASTAnots'.
 type TypeA    = Type    ASTAnots
+
+-- | 'ExprA': 'Expr' annotated with 'ASTAnots'.
 type ExprA    = Expr    ASTAnots
+
+-- | 'AddOpA': 'AddOp' annotated with 'ASTAnots'.
 type AddOpA   = AddOp   ASTAnots
+
+-- | 'MulOpA': 'MulOp' annotated with 'ASTAnots'.
 type MulOpA   = MulOp   ASTAnots
+
+-- | 'RelOpA': 'RelOp' annotated with 'ASTAnots'.
 type RelOpA   = RelOp   ASTAnots
