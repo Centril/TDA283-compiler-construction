3fc9b9835e173c280284d3b89af8371d1eebbb00
Mazdak Farrokhzad <twingoow@gmail.com>
2016-05-17 06:14:27 +0200

    Utils.Function: added applyN

--- src/Utils/Function.hs
+++ src/Utils/Function.hs
@@ -29,8 +29,12 @@ General utility for functions and arrows.
 -}
 module Utils.Function (
     -- * Operations
-    flip3, (§), (<§>), untilMatch, untilEq
+    flip3, (§), (<§>), untilMatch, untilEq, applyN
 ) where
+
+--import Utils.Pointless
+
+import Data.List
 
 -- | 'flip3': move the third argument of a function to the front.
 flip3 :: (a -> b -> c -> r) -> c -> a -> b -> r
@@ -59,3 +63,8 @@ untilMatch p f x
 -- | 'untilEq': specialization of 'untilMatch' for '(==)'
 untilEq :: Eq a => (a -> a) -> a -> a
 untilEq = untilMatch (==)
+
+-- | 'applyN': applies a function f, i times to x. Therefore applyN 0 id == id.
+applyN :: (Eq i, Num i) => i -> (a -> a) -> a -> a
+applyN 0 f x = x
+applyN i f x = applyN (i - 1) f $ f x
